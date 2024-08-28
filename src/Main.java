4e803c9d6bf382d5c84d980c7b3207c7b26091c0
isabellang <isabellayln2027@student.cis.edu.hk>
2024-08-28 09:33:04 +0800

    completed IDE quiz

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,29 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Izzy
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+
+        System.out.println("Enter your age:");
+        age = scanner.nextInt();
+        System.out.println("Enter your first name:");
+        scanner.nextLine();
+        firstName =  scanner.nextLine();
+        System.out.println("Enter your favourite food:");
+        favouriteFood = scanner.nextLine();
+
+        System.out.println("First Name: " + firstName + "\nAge: " + age + "\nFavourite Food: " + favouriteFood);
+
+
 
 
 
